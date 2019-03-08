868642f3dc231fcbec16a2318cc5c3c7dd5d3935
Marcelo Tournier <dr.tournier@gmail.com>
2019-03-08 13:26:32 -0800

    lasso model

--- Marcelo_birthweight.py
+++ Marcelo_birthweight.py
@@ -357,3 +357,101 @@ results2 = lm_babyweight2.fit()
 print(results2.summary())
 
 # R-squared reduced...
+
+
+
+
+####################################################
+# LASSO MODEL
+
+# Import Lasso
+from sklearn.linear_model import Lasso
+
+# Instantiate a lasso regressor: lasso
+lasso = Lasso(alpha=0.005,normalize=True)
+
+# Fit the regressor to the data
+lasso.fit(X, y)
+
+# Calling the score method, which compares the predicted values to the actual values
+
+y_score = lasso.score(X_test, y_test)
+
+# The score is directly comparable to R-Square
+print(y_score)
+
+#####
+# Compute and print the coefficients
+lasso_coef = lasso.coef_
+
+print(lasso_coef)
+
+# Plot the coefficients:
+# 
+plt.plot(range(len(X.columns)), lasso_coef)
+plt.xticks(range(len(X.columns)), X.columns.values, rotation=60)
+plt.margins(0.02)
+plt.show()
+
+
+######################################################
+# RIDGE REGRESSION MODEL
+
+# Import necessary modules
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import cross_val_score
+
+# Setup the array of alphas and lists to store scores
+alpha_space = np.logspace(-4, 0, 50)
+ridge_scores = []
+ridge_scores_std = []
+
+# Create a ridge regressor: ridge
+ridge = Ridge(normalize=True)
+
+ridge.alpha = 0.005
+
+ridge.fit(X, y)
+
+# Calling the score method, which compares the predicted values to the actual values
+
+y_score = ridge.score(X_test, y_test)
+
+# The score is directly comparable to R-Square
+print(y_score)
+
+# Compute scores over range of alphas
+for alpha in alpha_space:
+
+    # Specify the alpha value to use: ridge.alpha
+    ridge.alpha = alpha
+    
+    # Perform 10-fold CV: ridge_cv_scores
+    ridge_cv_scores = cross_val_score(ridge, X, y, cv=10)
+    
+    # Append the mean of ridge_cv_scores to ridge_scores
+    ridge_scores.append(np.mean(ridge_cv_scores))
+    
+    # Append the std of ridge_cv_scores to ridge_scores_std
+    ridge_scores_std.append(np.std(ridge_cv_scores))
+
+# Display the plot
+def display_plot(cv_scores, cv_scores_std):
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(alpha_space, cv_scores)
+
+    std_error = cv_scores_std / np.sqrt(10)
+
+    ax.fill_between(alpha_space, cv_scores + std_error, cv_scores - std_error, alpha=0.2)
+    ax.set_ylabel('CV Score +/- Std Error')
+    ax.set_xlabel('Alpha')
+    ax.axhline(np.max(cv_scores), linestyle='--', color='.5')
+    ax.set_xlim([alpha_space[0], alpha_space[-1]])
+    ax.set_xscale('log')
+    plt.show()
+
+
+display_plot(ridge_scores, ridge_scores_std)
+
+
