3167dfdbd8cb81c8b8b06ba9c389f935fb0bf05e
sziritt <47197083+sziritt@users.noreply.github.com>
2019-03-13 22:55:44 -0700

    Add files via upload

--- BW_draft_steph.py
+++ BW_draft_steph.py
@@ -118,8 +118,9 @@ def rand_jitter(arr):
 for col in birth_weight.columns:
     x = birth_weight[col]
     y = birth_weight['bwght']
+    plt.scatter(rand_jitter(x), y)
     plt.xlabel(col)
-    plt.ylabel("bwght")
+    plt.ylabel("Birth weight")
     plt.axhline(2500,color='blue')
     plt.axhline(4000,color='red')
     plt.show()
@@ -127,9 +128,10 @@ for col in birth_weight.columns:
 # Correlations:
 birth_weight.corr()['bwght'].sort_values()
 # Correlation matrix
+birth_weight.corr().round(2)
 
 # Classification: low, normal, high weight
-df = pd.DataFrame.copy(birth_weight)
+df = pd.DataFrame.copy(birth_weight) #create df just for weight classification
 
 df['wclass'] = 'norm_weight'
 df.loc[df.bwght < 2500,'wclass'] = 'lo_weight'
@@ -189,8 +191,8 @@ birth_weight['drinker'] = (birth_weight.drink > 0).astype('int')
 # Creating binary variable 'smoker'
 birth_weight['smoker'] = (birth_weight.cigs > 0).astype('int')
 
+# Combination of drinker and smoker
 birth_weight['trasher'] = birth_weight.drinker+birth_weight.smoker
-
 birth_weight.loc[birth_weight.trasher == 2,'trasher'] = 4
 
 # Binary outliers:
@@ -207,13 +209,10 @@ birth_weight['lo_out_npvis'] = (birth_weight.npvis <=10).astype('int')
 birth_weight['hi_out_npvis'] = (birth_weight.npvis > 15).astype('int')
 
 # Creating binary variable 'out_mage' # original=60 (2nd = 54)
-birth_weight['out_mage'] = (birth_weight.mage > 54).astype('int')
+birth_weight['mover_54'] = (birth_weight.mage > 54).astype('int')
 
-# INCLUDE MOTHER´S AGE > 65
-birth_weight['big_out_mage'] = (birth_weight.mage >64).astype('int')*3 
-
-# Creating binary variable 'out_fage' # original=55
-birth_weight['out_fage'] = (birth_weight.fage > 46).astype('int')
+# Creating binary variable 'out_fage' # original=55 (2nd = 46)
+birth_weight['fover_46'] = (birth_weight.fage > 46).astype('int')
 
 # Creating binary variable 'mcollege' - mothers who went to college
 birth_weight['mcollege'] = (birth_weight.meduc >= 14).astype('int')
@@ -221,31 +220,8 @@ birth_weight['mcollege'] = (birth_weight.meduc >= 14).astype('int')
 # Creating binary variable 'fcollege' - fathers who went to college
 birth_weight['fcollege'] = (birth_weight.feduc >= 14).astype('int')
 
-# Creating binary variable 'out_feduc' # original=7
-birth_weight['out_feduc'] = (birth_weight.feduc < 7).astype('int')
-
-# Creating binary variable 'out_monpre' # original=4
-birth_weight['out_monpre'] = (birth_weight.monpre > 4).astype('int')
-
-# New variable 'mage_medu' = ratio mother's age and education
-birth_weight['mage_medu'] = birth_weight.mage/birth_weight.meduc
-
-# New variable 'fage_fedu' = ratio father's age and education
-birth_weight['fage_fedu'] = birth_weight.fage/birth_weight.feduc
-
-# New variable 'cigs_mage' = force multiplier for drinking and age:
-# This means that older women who drink too much will be more penalized
-birth_weight['cigs_mage'] = birth_weight.cigs*birth_weight.mage
-
-# New variable 'drink_mage' = force multiplier for drinking and age:
-# This means that older women who drink too much will be more penalized
-birth_weight['drink_mage'] = birth_weight.drink*birth_weight.mage
-
-# New variable 'out_cigs_mage' = outlier of 'out_cigs_mage' (>590):
-birth_weight['out_cigs_mage'] = (birth_weight.monpre > 590).astype('int')
-
-# New variable 'out_drink_mage' = outlier of 'out_drink_mage' (>1250):
-birth_weight['out_drink_mage'] = (birth_weight.monpre > 1250).astype('int')
+# Getting the log value of father's education because of skewness 
+birth_weight['log_feduc']= np.log(birth_weight['feduc'])
 
 # Combine ages of mather and father
 """ 
@@ -321,20 +297,17 @@ for value in birth_weight['npvis']:
             birth_weight.loc[counter,'regular'] = 1 
     counter += 1
 
-# Getting the log value of father's education because of skewness 
-birth_weight['log_feduc']= np.log(birth_weight['feduc'])
-
 # Checking correlation with new variables
 birth_weight.corr()['bwght'].sort_values().round(2)
 
 ###############################################################################
-##### LINEAR REGRESSION USSING STATSMODELS
+##### LINEAR REGRESSION USING STATSMODELS
 ###############################################################################
 # Libraries
 import statsmodels.formula.api as smf 
 
 df = pd.DataFrame.copy(birth_weight)
-### OPTION 1 
+
 # Building a Regression Base
 lm_babyweight = smf.ols(formula = """bwght ~ df['mage'] +
                         df['meduc'] +
@@ -351,13 +324,21 @@ lm_babyweight = smf.ols(formula = """bwght ~ df['mage'] +
                         df['fwhte'] +
                         df['fblck'] +
                         df['foth'] +
+                        df['drinker'] +
+                        df['smoker'] + 
+                        df['trasher'] +
                         df['out_drink'] +
+                        df['out_cigs'] +
                         df['lo_out_npvis'] +
                         df['hi_out_npvis'] +
-                        df['out_mage'] +
-                        df['out_fage'] +
-                        df['out_feduc'] +
-                        df['out_monpre'] """,
+                        df['mover_54'] +
+                        df['fover_46'] +
+                        df['mcollege'] +
+                        df['fcollege'] +
+                        df['log_feduc'] +
+                        df['cage'] +
+                        df['regular']
+                        """,
                         data = df)
 
 # Fitting Results
@@ -367,22 +348,103 @@ results = lm_babyweight.fit()
 print(results.summary())
 
 ###############################################################################
-##### 
+##### TESTING KNN
 ###############################################################################
+# Additional Libraries
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_squared_error 
+from sklearn.model_selection import train_test_split 
 
-###############################################################################
-##### 
-###############################################################################
+def test_knn(variables,knn):
+    bb_data = df.loc[:,variables]
+    
+    bb_target = df.loc[:,'bwght']
 
+    Xf_train, Xf_test, yf_train, yf_test = train_test_split(
+                                                        bb_data,bb_target, 
+                                                        test_size = 0.1, 
+                                                        random_state=508)
+    for k in range(1,knn):
+        print('Number of neighbors : '+str(k))
+        bb = KNeighborsRegressor(algorithm = 'auto',
+                                  n_neighbors = k)
+        bb.fit(Xf_train,yf_train)
 
+        # Compute and print R^2 and RMSE
+        yf_pred_reg2 = bb.predict(Xf_test)
+        print('\tR-Squared (train set): ',bb.score(Xf_train,yf_train).round(3))
+        print('\tR-Squared  (test set): ',bb.score(Xf_test,yf_test).round(3))
+        rmse = np.sqrt(mean_squared_error(yf_test , yf_pred_reg2))
+        print("\tRoot Mean Squared Error: {}\n".format(rmse))
 
-
-
-
-
+# KNN Model with Rˆ2 = 0.614 with k=7
+test_knn(['mage','fage','cigs','drink','npvis','lo_out_npvis','out_drink'],15)
 
 ###############################################################################
 ##### TEST MODELS FUNCTION
+###############################################################################
+# Additional Libraries
+from sklearn.linear_model import LinearRegression 
+
+# Define function to test diferrent models
+def test_regression(variables):
+    values = str()
+    for var in variables: 
+        if var == variables[-1]:
+            values = values + "birth_weight['"+var+"']"
+        else:
+            values = values + "birth_weight['"+var+"'] +"
+
+    v4_baby = smf.ols(formula = "bwght ~ "+values, data=birth_weight)
+
+    results_v4 = v4_baby.fit()
+
+    print(results_v4.summary())
+    
+    print("\n####################################\n")
+    
+    print("## Testing variables in sklearn:\n")
+    
+    bb_data = birth_weight.loc[:,variables]
+    
+    bb_target = birth_weight.loc[:,'bwght']
+
+    Xf_train, Xf_test, yf_train, yf_test = train_test_split(
+                                                        bb_data,bb_target, 
+                                                        test_size=0.1, 
+                                                        random_state=508)
+    bb = LinearRegression()
+    bb.fit(Xf_train, yf_train)
+
+    # Compute and print R^2 and RMSE
+    yf_pred_reg2 = bb.predict(Xf_test)
+    print('R-Squared: ', results_v4.rsquared.round(3))
+    print('Training score: ', bb.score(Xf_train,yf_train).round(3))
+    print('Testing Score: ', bb.score(Xf_test,yf_test).round(3))
+    
+    rmse = np.sqrt(mean_squared_error(yf_test , yf_pred_reg2))
+    print("Root Mean Squared Error: {}".format(rmse))
+
+
+# Using testing function:
+# All original variables
+test_regression(['mage', 'meduc', 'monpre', 'npvis', 'fage', 'feduc', 'cigs', 
+                 'drink','male', 'mwhte', 'mblck', 'moth', 'fwhte', 'fblck', 
+                 'foth'])
+
+# Testing only wiht significant variables
+test_regression(['mage', 'cigs','drink', 'mwhte', 'mblck', 'moth', 'fwhte', 
+                 'fblck', 'foth'])
+    
+# Significant and more correlated variables + outliers
+test_regression(['mage','cigs','drink','lo_out_npvis',
+                 'hi_out_npvis','out_drink','drinker'])
+# Final model
+test_regression(['cigs','drink','mover_54',
+                 'fage','log_feduc'])
+
+###############################################################################
+##### FINAL MODEL - BEST OVERALL
 ###############################################################################
 # Additional Libraries
 from sklearn.linear_model import LinearRegression 
@@ -394,11 +456,11 @@ def test_regression(variables):
     values = str()
     for var in variables: 
         if var == variables[-1]:
-            values = values + "df['"+var+"']"
+            values = values + "birth_weight['"+var+"']"
         else:
-            values = values + "df['"+var+"'] +"
+            values = values + "birth_weight['"+var+"'] +"
 
-    v4_baby = smf.ols(formula = "bwght ~ "+values,data=df)
+    v4_baby = smf.ols(formula = "bwght ~ "+values, data=birth_weight)
 
     results_v4 = v4_baby.fit()
 
@@ -408,25 +470,26 @@ def test_regression(variables):
     
     print("## Testing variables in sklearn:\n")
     
-    bb_data = df.loc[:,variables]
+    bb_data = birth_weight.loc[:,variables]
     
-    bb_target = df.loc[:,'bwght']
+    bb_target = birth_weight.loc[:,'bwght']
 
     Xf_train, Xf_test, yf_train, yf_test = train_test_split(
                                                         bb_data,bb_target, 
-                                                        test_size = 0.1, 
+                                                        test_size=0.1, 
                                                         random_state=508)
     bb = LinearRegression()
     bb.fit(Xf_train, yf_train)
 
     # Compute and print R^2 and RMSE
     yf_pred_reg2 = bb.predict(Xf_test)
-    print('R-Squared: ', bb.score(Xf_test,yf_test).round(3))
+    print('R-Squared: ', results_v4.rsquared.round(3))
+    print('Training score: ', bb.score(Xf_train,yf_train).round(3))
+    print('Testing Score: ', bb.score(Xf_test,yf_test).round(3))
+    
     rmse = np.sqrt(mean_squared_error(yf_test , yf_pred_reg2))
     print("Root Mean Squared Error: {}".format(rmse))
-
-# Using testing function:
-test_regression(['mage','cigs','drink','lo_out_npvis',
-                 'hi_out_npvis','out_drink','drinker'])
-
-
+    
+    
+test_regression(['cigs','drink','mover_54',
+                 'fage','log_feduc'])
